f5817283d0fffda12800b2e135bf878a15866c57
aldairl <aldairl@unicauca.edu.co>
2019-10-06 20:32:38 -0500

    table event created

--- user_events/models.py
+++ user_events/models.py
@@ -1,6 +1,21 @@
 from django.db import models
 
 # Create your models here.
-#class Event(models.model):
+class Event(models.Model):
+
+    id_user = models.CharField(max_length=50, blank=True, null=True)
+    ip = models.CharField(max_length=13, blank=True, null=True)
+    username = models.CharField(max_length = 50, blank = True, null = True)
+    display_name = models.CharField(max_length = 50, blank = True, null = True)
+    date = models.DateTimeField(blank = False)
+
+    page = models.CharField(max_length = 150, blank = True, null = True)
+    section = models.CharField(max_length = 150, blank = True, null = True)
+    subsection = models.CharField(max_length = 150, blank = True, null = True)
+    unit = models.CharField(max_length = 150, blank = True, null = True)
+    session = models.CharField(max_length = 150, blank = True, null = True)
+    course = models.CharField(max_length = 150, blank = True, null = True)
+    answers = models.CharField(max_length = 150, blank = True, null = True)
+
 
 	
